87f0e96c9a44cf2460568c2a03eca9e7984775bf
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-18 11:17:31 +0100

    Add loop to event listener so every button will be shown in the display element.

--- javascript.js
+++ javascript.js
@@ -4,10 +4,24 @@ let display_number = 0;
 // successfully showing display_number on webpage
 // display.innerHTML = display_number;
 
-document.querySelector(".inputButton").addEventListener("click", event =>{
+// document.querySelector(".inputButton").addEventListener("click", event =>{
+//     let target = event.target;
+//     if(target.matches("button")){
+//         let value = target.innerHTML
+//         document.querySelector(".display").value += value;
+//     }
+// })
+
+// trying to fetch all buttons
+const inputButtons = document.querySelectorAll(".inputButton");
+length_buttons = inputButtons.length;
+
+for(i=0; i<length_buttons; i++){
+    inputButtons[i].addEventListener("click", event =>{
     let target = event.target;
+    
     if(target.matches("button")){
         let value = target.innerHTML
         document.querySelector(".display").value += value;
     }
-})
+})}
