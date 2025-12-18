c12213fb4d5aff2efd3a49bc26d3f697430c0077
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-18 11:03:48 +0100

    Change display element from div to input. Add event listener so value of button is shown on display. Right now only works for first inputButton.

--- javascript.js
+++ javascript.js
@@ -2,4 +2,12 @@ const display = document.querySelector('.display');
 let display_number = 0;
 
 // successfully showing display_number on webpage
-display.innerHTML = display_number;
+// display.innerHTML = display_number;
+
+document.querySelector(".inputButton").addEventListener("click", event =>{
+    let target = event.target;
+    if(target.matches("button")){
+        let value = target.innerHTML
+        document.querySelector(".display").value += value;
+    }
+})
